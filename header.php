19bc4fc9c9dfe158adb8fdeafe8081bf52353c76
Timothy Long <timothydavidlong@gmail.com>
2015-08-31 07:48:03 -0700

    Go Crimson Text!

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 	<meta name="author" content="Timothy Long">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-	<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,700,400italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Crimson+Text:400,400italic,700">
 	<link rel="alternate" href="<?php bloginfo('rss2_url'); ?>" />
 	<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
